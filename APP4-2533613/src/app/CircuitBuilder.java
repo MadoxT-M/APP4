73fb8c30a6f4987d63f0f1acf76d3945ce429b55
Madox Tinoco <2533613@cegeplimoilou.ca>
2026-03-31 12:23:46 -0400

    Creation du methode LireCircuit dans la classe de CircuitBuilder

--- APP4-2533613/src/app/CircuitBuilder.java
+++ APP4-2533613/src/app/CircuitBuilder.java
@@ -1,4 +1,41 @@
 package app;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import electronique.CircuitParallele;
+import electronique.CircuitSerie;
+import electronique.Composant;
+import electronique.Resistance;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CircuitBuilder {
+
+    private Composant lireCircuit(JsonNode node) {
+        String type = node.get("type").asText();
+
+        if ("resistance".equals(type)) {
+
+            return new Resistance(node.get("value").asDouble());
+
+        } else if ("parallelle".equals(type)) {
+
+            List<Composant> composants = new ArrayList<>();
+
+            for(JsonNode composantNode : node.get("composants")) {
+                composants.add(lireCircuit(composantNode));
+            }
+            return new CircuitParallele(composants);
+
+        } else if ("serie".equals(type)) {
+
+            List<Composant> composants = new ArrayList<>();
+
+            for(JsonNode composantNode : node.get("composants")) {
+                composants.add(lireCircuit(composantNode));
+            }
+            return new CircuitSerie(composants);
+        }
+        throw new IllegalArgumentException("Invalid type: " + type);
+    }
 }

377a9e9f3b683977752a65507cd7429e858b91fb
Madox Tinoco <2533613@cegeplimoilou.ca>
2026-03-31 12:03:43 -0400

    Ajout constructeur et @Override calculerResistance dans la classe CircuitParallele

--- APP4-2533613/src/electronique/CircuitParallele.java
+++ APP4-2533613/src/electronique/CircuitParallele.java
@@ -1,4 +1,18 @@
 package electronique;
 
-public class CircuitParallele {
+import java.util.List;
+
+public class CircuitParallele extends Circuit {
+
+    public CircuitParallele(List<Composant> composants) {super(composants);}
+
+    @Override
+    public double calculerResistance() {
+        double resistanceParallelle = 0.0;
+        for (Composant c : composants) {
+            resistanceParallelle += 1/c.calculerResistance();
+        }
+        return 1/ resistanceParallelle;
+    }
+
 }

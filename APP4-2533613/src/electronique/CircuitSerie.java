93afee02f9c62176838b89e405928d77c98a0a37
Madox Tinoco <2533613@cegeplimoilou.ca>
2026-03-31 11:58:10 -0400

    Ajout @Override calculerResistance dans la classe CircuitSerie

--- APP4-2533613/src/electronique/CircuitSerie.java
+++ APP4-2533613/src/electronique/CircuitSerie.java
@@ -3,7 +3,19 @@ package electronique;
 import java.util.List;
 
 public class CircuitSerie extends Circuit{
+
+
     public CircuitSerie(List<Composant> composants) {
         super(composants);
+    }
+
+
+    @Override
+    public double calculerResistance() {
+        double totalResistance = 0.0;
+        for(Composant c : composants) {
+            totalResistance += c.calculerResistance();
+        }
+        return totalResistance;
     }
 }

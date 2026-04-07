de52df0e6a474a6e5c730fe074f84d98ca7532bc
Madox Tinoco Munoz <2533613@cegeplimoilou.ca>
2026-04-06 23:38:15 -0400

    Creation methodes Main, lireChoix et finCycle dans la classe CircuitApp

--- APP4-2533613/src/app/CircuitApp.java
+++ APP4-2533613/src/app/CircuitApp.java
@@ -1,5 +1,9 @@
 package app;
 
+import electronique.Composant;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class CircuitApp {
@@ -7,6 +11,82 @@ public class CircuitApp {
 
     static void main() {
 
+        boolean continuer = true;
 
+        while (continuer) {
+            File donnees = new File("APP4-2533613/donnees/fichiers_json");
+            File[] fichiers = donnees.listFiles((dir, name) -> name.endsWith(".json"));
+
+            if(fichiers == null || fichiers.length == 0) {
+                System.out.println("Aucun fichier JSON trouvé");
+                return;
+            }
+
+            System.out.println("--------- Sélectionnez un circuit ---------");
+            for(int i = 0; i < fichiers.length; i++) {
+                System.out.println("[" + (i+1) + "] " + fichiers[i].getName());
+            }
+
+            int choix = lireChoix(fichiers.length);
+
+            File fichierChoix = fichiers[choix-1];
+
+            try {
+                CircuitBuilder cb = new CircuitBuilder();
+                Composant circuit = cb.construireCircuit(fichierChoix.getPath());
+
+                double resultat = circuit.calculerResistance();
+
+                System.out.println("\nRésistance équivalente : " + resultat + "Ω");
+
+            } catch (IOException e) {
+                System.out.println("Erreur : " + e.getMessage());
+            }
+
+            continuer = finCycle();
+        }
+
+        System.out.println("Programme terminé.");
+
+    }
+
+    private static int lireChoix(int choixMax){
+        int choix= -1;
+
+        while(true) {
+            System.out.print("Votre choix : ");
+
+            if (sc.hasNextInt()) {
+                choix = sc.nextInt();
+                sc.nextLine();
+
+                if (choix >= 1  && choix <= choixMax) {
+                    return choix;
+                }
+            } else {
+                sc.nextLine();
+            }
+
+            System.out.println("Choix d'entrée invalide. Réessayez.");
+        }
+    }
+
+    private static boolean finCycle() {
+
+        while(true) {
+            System.out.println("\n[R] Recommencer");
+            System.out.println("[Q] Quitter");
+            System.out.print("Choix : ");
+
+            String choix = sc.nextLine().trim().toUpperCase();
+
+            if (choix.equals("R")) {
+                return true;
+            } else if (choix.equals("Q")) {
+                return false;
+            }  else {
+                System.out.println("Action invalide. ");
+            }
+        }
     }
 }
